aa660ca7525a90136d3821d0944ca35a81f9d01b
drinkingkazu <kterao@slac.stanford.edu>
2024-08-22 10:29:34 -0700

    adopt newer version of rootcling dictionary generation. currently loading so lib explicitly using gSystem.Load but we should not have to do this... something to be fixed for cling dict gen

--- python/flashmatch/__init__.py
+++ python/flashmatch/__init__.py
@@ -1,3 +1,8 @@
+import os
+if not 'FMATCH_LIBDIR' in os.environ:
+    raise ImportError('FMATCH_LIBDIR environment variable must be defined.')
+from ROOT import gSystem
+gSystem.Load(os.path.join(os.environ['FMATCH_LIBDIR'],'libflashmatch.so'))
 from ROOT import flashmatch, phot, sim, geoalgo
 #force loading C functions in dict by instantiating a class
 c=flashmatch.PSet
